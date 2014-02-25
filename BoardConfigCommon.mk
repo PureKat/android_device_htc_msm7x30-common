ad0fa1264b5095ac0dda7646c925d215540432ba
Vladislav Koldobskiy <admin@nevergone.ru>
2014-02-25 19:46:02 +0400

    Switch SELinux to Enforcing mode
    https://dl.dropboxusercontent.com/u/16268774/Screenshots/Phone/Screenshot_2014-02-25-19-42-57.png

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,7 +42,7 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/htc/msm7x30-3.0
-BOARD_KERNEL_CMDLINE := no_console_suspend=1 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := no_console_suspend=1
 BOARD_KERNEL_RECOVERY_CMDLINE := $(BOARD_KERNEL_CMDLINE) msmsdcc_power_gpio=88
 BOARD_KERNEL_PAGE_SIZE := 4096
 TARGET_NO_BOOTLOADER := true
